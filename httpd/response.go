68b09b9e400f6bd88ece97e94335b34afd8ce223
Yizhe Qin <yizheqin@yizhes-mac-mini.lan>
2022-05-19 09:02:58 -0700

    2.2 Fix bug

--- httpd/response.go
+++ httpd/response.go
@@ -1,5 +1,17 @@
 package httpd
 
-type response struct{}
+type response struct {
+	c *conn
+}
 
-type ResponseWriter interface{}
+type ResponseWriter interface {
+	Write([]byte) (n int, err error)
+}
+
+func setupResponse(c *conn) *response {
+	return &response{c: c}
+}
+
+func (w *response) Write(p []byte) (int, error) {
+	return w.c.bufw.Write(p)
+}
